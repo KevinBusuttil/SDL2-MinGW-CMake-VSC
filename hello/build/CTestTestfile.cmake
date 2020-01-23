c90a518d88b0ffeb2f6226a1c80f0ef3e8d96a4c
Kevin Busuttil <kevin.busuttil@busuttil-technologies.com>
2020-01-23 19:49:29 +0100

    Add files via upload

---
+++ hello/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Kevin/Projects/CPP/hello
+# Build directory: C:/Users/Kevin/Projects/CPP/hello/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
