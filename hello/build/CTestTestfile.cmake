01cb976c18d9228646daa46f729a57a28ad18442
Kevin Busuttil <kevin.busuttil@busuttil-technologies.com>
2020-01-23 20:00:27 +0100

    Not necessary

--- hello/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kevin/Projects/CPP/hello
-# Build directory: C:/Users/Kevin/Projects/CPP/hello/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
